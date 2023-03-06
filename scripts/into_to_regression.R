c77f32dfe0bfedc7b0fe37b6550af11638b87d9c
wpb21mwu <wpb21mwu@uea.ac.uk>
2023-03-06 18:50:34 +0000

    Adding Assumptions to regressions

--- scripts/into_to_regression.R
+++ scripts/into_to_regression.R
@@ -54,3 +54,61 @@ janka %>%
   mutate(centered_dens = dens-pull(dens_mean)) %>% 
   lm(hardness ~ centered_dens, data = .) %>% 
   broom::tidy()
+
+broom::tidy(janka_ls1, conf.int=T, conf.level=0.95)
+
+janka_ls1 %>% 
+  broom::glance()
+#___________________----
+
+#Assumptions----
+
+janka_ls1 %>% 
+  broom::augment() %>% 
+  head()
+
+augmented_ls1 <- janka_ls1 %>% 
+  broom::augment()
+
+augmented_ls1 %>% 
+  ggplot(aes(x=dens, 
+             y=.fitted))+
+  geom_line()+ 
+  geom_point(aes(x=dens, 
+                 y=hardness))+
+  geom_segment(aes(x=dens, 
+                   xend=dens, 
+                   y=.fitted, 
+                   yend=hardness), 
+               linetype="dashed", colour="red") # Shows residuals from the regression line. A perfect correlation will have 0 residuals. This is very unlikely instead we are likely to see 1.that there is a 'normal distribution' to the residuals e.g. more residuals close to the mean, and fewer further away in a rough z-distribution. 2. We also want to see homogeneity of the residuals e.g. it would be a bad model if the average error was greater at one end of the model than the other. This might mean we have more uncertainty in the slope of the line for large values over small values or vice versa.
+
+
+# A line connecting all the data points in order 
+p1 <- augmented_ls1 %>% 
+  ggplot(aes(x=dens, y=hardness))+
+  geom_line()+
+  ggtitle("Full Data")
+
+# Plotting the fitted values against the independent e.g. our regression line
+p2 <- augmented_ls1 %>% 
+  ggplot(aes(x=dens, y=.fitted))+
+  geom_line()+
+  ggtitle("Linear trend")
+
+# Plotting the residuals against the fitted values e.g. remaining variance
+p3 <- augmented_ls1 %>% 
+  ggplot(aes(x=.fitted, y=.resid))+
+  geom_hline(yintercept=0, colour="white", size=5)+
+  geom_line()+
+  ggtitle("Remaining \npattern")
+
+
+library(patchwork)
+p1+p2+p3
+
+
+
+
+
+
+
